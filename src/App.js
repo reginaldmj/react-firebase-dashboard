90feea345244f9ca47f1c63404185b913115bc3f
Reginald Jefferson <reginald.jeffersonm@gmail.com>
2020-10-10 18:08:47 -0500

    working with fb

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import Header from './Header';
 import './App.css';
 import './firebase/config';
+import {Signup} from '/Signup'
+import  { Route, Switch, BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <Header></Header>
       <div className="app">
         <div className="ui grid container"></div>
+          <Switch>
+            <Route exact path ="/signup" component={Signup} />
+          </Switch>
       </div>
     </div>
+    </BrowserRouter>
   );
 }
-
+ 
 export default App;
